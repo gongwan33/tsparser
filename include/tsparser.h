5530db3919b0934f1551bdfa2434161ad49807e4
wagner <gongwan33@gmail.com>
2015-07-24 13:24:29 +0800

    pat start

--- include/tsparser.h
+++ include/tsparser.h
@@ -11,7 +11,8 @@
 #define TRUE 1
 #define FALSE 0
 
-#define FILE_BUFFER_SIZE 10*1024
+#define FILE_BUFFER_SIZE 100*1024
+#define PACK_BUFFER_SIZE 4*1024
 
 char isFreqAvailable(int freq);
 char startParse();
@@ -22,7 +23,7 @@ char parseTS(int freq);
 
 struct tsHeader
 {
-	char sync_byte:									8;
+	unsigned char sync_byte:						8;
 	unsigned int transport_error_indicator:			1;
 	unsigned int payload_unit_start_indicator:		1;
 	unsigned int transport_priority:				1;
@@ -31,6 +32,122 @@ struct tsHeader
 	unsigned int adaptation_field_control:			2;
 	unsigned int continuity_counter:				4;	
 } header;
+
+struct PCR
+{
+	unsigned long long int program_clock_reference_base:			33;
+	unsigned long long int reserved:								6;
+	unsigned long long int program_clock_reference_extension:		9;
+};
+
+struct OPCR
+{
+	unsigned long long int original_program_clock_reference_base:		33;
+	unsigned long long int reserved:									6;
+	unsigned long long int original_program_clock_reference_extension:	9; 
+};
+
+struct ltw
+{
+	unsigned int ltw_valid_flag:		1;
+	unsigned int ltw_offset:			15;
+};
+
+struct piecewiseRate
+{
+	unsigned int reserved:				2;
+	unsigned int piecewise_rate:		22;
+};
+
+struct seamlessSplice
+{
+	unsigned char splice_type:			4;
+	unsigned char DTS_next_AU32_30:		3;
+	unsigned char marker_bit0:			1;
+	unsigned int DTS_next_AU29_15:		15;
+	unsigned int marker_bit1:			1;
+	unsigned int DTS_net_AU14_0:		15;
+	unsigned int marker_bit2:			1;
+};
+
+struct adaptationFieldExtension
+{
+	unsigned char adaptation_field_extension_length:			8;
+	unsigned char ltw_flag:										1;
+	unsigned char piecewise_rate_flag:							1;
+	unsigned char seamless_splice_flag:							1;
+	unsigned char reserved:										5;
+	struct ltw ltwElm;
+	struct piecewiseRate piecewiseElm;
+	struct seamlessSplice seamlessElm;
+};
+
+struct adaptationField
+{
+	unsigned char adaptation_field_length:				8;
+	unsigned char discontinuity_indicator:			    1;
+	unsigned char random_access_indicator:				1;
+	unsigned char elementary_stream_priority_indicator:	1;
+	unsigned char PCR_flag:								1;
+	unsigned char OPCR_flag:							1;
+	unsigned char splicing_point_flag:					1;
+	unsigned char transport_private_data_flag:			1;
+	unsigned char adaptation_field_extension_flag:		1;
+	struct PCR PCRElm;
+	struct OPCR OPCRElm;
+	unsigned char splice_countdown:						8;
+	unsigned char tansport_private_data_length:			8;
+} aField;
+
+struct PES
+{
+	unsigned int packet_start_code_prefix:				24;
+	unsigned int stream_id:								8;
+	unsigned int PES_packet_length:						16;
+	unsigned char PES_scrambling_control:				2;
+	unsigned char PES_priority:							1;
+	unsigned char data_alignment_indicator:				1;
+	unsigned char copyright:							1;
+	unsigned char original_or_copy:						1;
+	unsigned char PTS_DTS_flags:						2;
+	unsigned int ESCR_flag:								1;
+	unsigned int ES_rate_flag:							1;
+	unsigned int DSM_trick_mode_flag:					1;
+	unsigned int additional_copy_info_flag:				1;
+	unsigned int PES_CRC_flag:							1;
+	unsigned int PES_extension_flag:					1;
+	unsigned int PES_header_data_length:				8;
+	unsigned int PTS;
+	unsigned int DTS;
+	unsigned int ESCR_base;
+	unsigned int ES_rate;
+} PESheader;
+
+struct programInfo
+{
+	unsigned int program_number:				16;
+	unsigned int reserved:						3;
+	unsigned int network_program_PID:			13;
+};
+
+struct PAT
+{
+	unsigned char table_id:						8;
+	unsigned char section_syntax_indicator:		1;
+	unsigned int section_length:				12;
+	unsigned int transport_stream_id:			16;
+	unsigned char version_number:				5;
+	unsigned char current_next_indicator:		1;
+	unsigned char section_number:				8;
+	unsigned char last_section_number:			8;
+	struct programInfo programInfoElm;
+	unsigned int CRC_32;
+} PATElm;
+
+struct pointerField
+{
+	unsigned char pointer_field;
+} pointerFieldElm;
 
 #endif
 
--- tsparser.c
+++ tsparser.c
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <tsparser.h>
 #include <utils.h>
@@ -14,16 +15,20 @@
 #define PRINT_DEBUG
 
 FILE* tsFp;
-char* fileBuffer;
+unsigned char* fileBuffer;
 int fileBufferP;
 int fileBufferLen;
+unsigned char* packBuffer;
+int packBufferP;
+int packBufferLen;
+char PATOkFlag = -1;
+char PMTOkFlag = -1;
 
 char isFreqAvailable(int freq)
 {
 	char fileName[200];
 	sprintf(fileName, "../analyseTS_data/ocn_%d.ts", freq);
 	tsFp = fopen(fileName, "rb");
-	printf("fp=%p\n", tsFp);
 	if(tsFp != NULL)
 	{
 		fclose(tsFp);
@@ -35,13 +40,19 @@ char isFreqAvailable(int freq)
 
 char startParse()
 {
-	fileBuffer = (char*)malloc(FILE_BUFFER_SIZE);	
+	fileBuffer = (unsigned char*)malloc(FILE_BUFFER_SIZE);	
+	packBuffer = (unsigned char*)malloc(PACK_BUFFER_SIZE);
+
+	memset(&header, 0, sizeof(header));
 }
 
 char endParse()
 {
 	if(fileBuffer != NULL)
 		free(fileBuffer);			
+
+	if(packBuffer != NULL)
+		free(packBuffer);
 }
 
 int openTSFile(int freq)
@@ -68,52 +79,162 @@ int closeTSFile()
 	return ret;
 }
 
-int analyseTSPacket(char *buffer, int* bufferP, int bufferLen)
+static void printStructInfo(int flag)
+{
+	switch(flag)
+	{
+		case 0:
+			printf("---------------------------------------------\n");
+			printf("Header Info:\n");
+			printf("sync_byte:\t 0x%x\n", header.sync_byte);
+			printf("transport_error_indicator:\t 0x%x\n", header.transport_error_indicator);
+			printf("payload_unit_start_indicator:\t 0x%x\n", header.payload_unit_start_indicator);
+			printf("transport_priority:\t 0x%x\n", header.transport_priority);
+			printf("PID:\t 0x%x\n", header.PID);
+			printf("transport_scrambling_control:\t 0x%x\n", header.transport_scrambling_control);
+			printf("adaptation_field_control:\t 0x%x\n", header.adaptation_field_control);
+			printf("continuity_counter:\t 0x%x\n", header.continuity_counter);
+			printf("---------------------------------------------\n");
+			break;
+
+		default:
+			printf("In printStructInfo: unknow flag!\n");
+	}
+}
+
+static int parsePAT(unsigned char* buffer, int bufferLen, int offset)
+{
+	if(bufferLen <= offset + 7)
+		return FALSE;
+
+	PATElm.table_id = buffer[offset];
+	PATElm.section_syntax_indicator = buffer[offset + 1] & 0x80;
+	PATElm.section_length = (buffer[offset + 1] & 0x0f) | (buffer[offset + 2] & 0xff);
+	PATElm.transport_stream_id = buffer[offset + 3] | buffer[offset + 4];
+	PATElm.version_number = (buffer[offset + 5] & 0x3e) >> 1;
+	PATElm.current_next_indicator = (buffer[offset + 5] & 0x01);
+	PATElm.section_number = buffer[offset + 6];
+	PATElm.last_section_number = buffer[offset + 7];
+
+	return TRUE;
+}
+
+static int analysePacket(unsigned char* buffer, int bufferLen)
+{
+	int offset = 4;
+
+	if(bufferLen <= offset)
+		return FALSE;
+
+	header.sync_byte = buffer[0];
+	header.transport_error_indicator = (buffer[1] & 0x80) >> 7;
+	header.payload_unit_start_indicator = (buffer[1] & 0x40) >> 6;
+	header.transport_priority = (buffer[1] & 0x20) >> 5;
+	header.PID = (buffer[1] & 0x1f) << 8 | buffer[2];
+	header.transport_scrambling_control = (buffer[3] & 0xc0) >> 6;
+	header.adaptation_field_control = (buffer[3] & 0x30) >> 4;
+	header.continuity_counter = buffer[3] & 0x0f;
+
+
+	if(header.PID != 0x0000 && PATOkFlag == -1)
+		return TRUE;
+
+	if(header.adaptation_field_control == 0x2 || header.adaptation_field_control == 0x3)
+	{
+		if(bufferLen <= offset + 1)
+			return FALSE;
+
+		aField.adaptation_field_length = buffer[offset];
+
+		offset = offset + aField.adaptation_field_length + 1;
+	}
+
+	if(header.adaptation_field_control == 0x1 || header.adaptation_field_control == 0x3)
+	{
+		if(header.payload_unit_start_indicator == 0x1)
+		{
+			if(bufferLen <= offset + 1)
+				return FALSE;
+			
+			pointerFieldElm.pointer_field = buffer[offset];		
+
+			offset = offset + 1 + pointerFieldElm.pointer_field;			
+		}
+
+		switch(header.PID)
+		{
+			case 0x0000:
+			parsePAT(buffer, bufferLen, offset);	
+			break;
+
+		}
+	}	
+
+	return TRUE;
+}
+//-------------------------------T
+int i = 0;
+//--------------------------------
+static int cutTSPacket(unsigned char* buffer, int* bufferP, int bufferLen)
 {
 	int p = 0;
 	int baseP = 0;
 	
-
 	if(*bufferP < 0 || bufferLen < 0 || buffer == NULL)
 		return FALSE;
 
-	p = *bufferP;
-
-	while(p < bufferLen)
+	while(p + baseP < bufferLen)
 	{
 		if(buffer[p + baseP] == 0x47)
 		{
+			if(p < PACK_BUFFER_SIZE)
+			{
+				memcpy(packBuffer, buffer + baseP, p);
+				analysePacket(packBuffer, p);
 #ifdef PRINT_DEBUG
-			printf("sync_byte found\n");
+//			printStructInfo(0);
 #endif
+			}
+			else
+				printf("Special pack size!\n");
+
+
 			baseP = p + baseP;
 			p = 0;
+//------------------------------------------------------------T
+i++;
+if(i > 1000)
+break;
+//-------------------------------------------------------------
 		}
-
-
-
+		
 		p++;
 	}
 
+	*bufferP = baseP;	
 	return TRUE;
 }
 
 char parseTS(int freq)
 {
+	int readLen = 0;
+
 	if(openTSFile(freq) != TRUE)
 		return FALSE;
 
 	while(1)
 	{
-		fileBufferP = 0;
-		if((fileBufferLen = fread(fileBuffer, FILE_BUFFER_SIZE, 1, tsFp)) <= 0)
+		if((readLen = fread(fileBuffer + fileBufferP, sizeof(unsigned char), FILE_BUFFER_SIZE - fileBufferP, tsFp)) <= 0)
 			break;
-		
-		if(!analyseTSPacket(fileBuffer, &fileBufferP, fileBufferLen))
+
+		fileBufferLen = readLen + fileBufferP;
+		if(!cutTSPacket(fileBuffer, &fileBufferP, fileBufferLen))
 		{
 			break;	
 		}
-
+		
+		memcpy(fileBuffer, fileBuffer + fileBufferP, fileBufferLen - fileBufferP);
+		fileBufferP = fileBufferLen - fileBufferP;
 	}
 	
 	closeTSFile();
